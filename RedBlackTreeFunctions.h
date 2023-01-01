c9ec0a0caf848452f2b493b7a4da6481d980cd5b
sek0ner <sek0ner@hotmail.com>
2023-01-01 23:39:34 +0200

    implemented leftRotate,rightRotate

--- RedBlackTreeFunctions.h
+++ RedBlackTreeFunctions.h
@@ -5,15 +5,20 @@
 #ifndef REDBLACKTREE_REDBLACKTREEFUNCTIONS_H
 #define REDBLACKTREE_REDBLACKTREEFUNCTIONS_H
 
+#include <stdbool.h>
 #include "Node.h"
 
 int getInsertValuesFromUser();
 
 void insertNodeFixup(Node *rootNode, Node z);
 
-void leftRotate(Node *rootNode, Node z);
+void leftRotate(Node *rootNode, Node x);
 
-void rightRotate(Node *rootNode, Node z);
+void rightRotate(Node *rootNode, Node y);
+
+bool isLeftChild(Node node);
+
+bool isRightChild(Node node);
 
 void insertNode(Node *rootNode) {
     Node newNode = initializeNewNode(getInsertValuesFromUser(), Red);
@@ -74,13 +79,44 @@ void insertNodeFixup(Node *rootNode, Node z) {
     (*rootNode)->colour = Black;
 }
 
-void rightRotate(Node *rootNode, Node z) {
 
+void rightRotate(Node *rootNode, Node y) {
+    Node x = y->left;
+    y->left = x->right;
+    if (x->right != nullNode) {
+        x->right->parent = y;
+    }
+    x->parent = y->parent;
+    if (y->parent == nullNode) {
+        (*rootNode) = x;
+    } else if (isLeftChild(y)) {
+        y->parent->left = x;
+    } else {
+        y->parent->right = x;
+    }
+    x->right = y;
+    y->parent = x;
 }
 
-void leftRotate(Node *rootNode, Node z) {
+void leftRotate(Node *rootNode, Node x) {
 //The pseudocode for LEFT-ROTATE assumes that x.right is not T.nil and that the
 //root’s parent is T.nil.
+    Node y = x->right;
+    x->right = y->left;
+    if (y->left != nullNode) {
+        y->left->parent = x;
+    }
+    y->parent = x->parent;
+    if (x->parent == nullNode) {
+        (*rootNode) = y;
+    } else if (isLeftChild(x)) {
+        x->parent->left = y;
+    } else {
+        x->parent->right = y;
+    }
+    y->left = x;
+    x->parent = y;
+
 }
 
 int getInsertValuesFromUser() {
@@ -97,6 +133,14 @@ void printTree(Node root) {
 
 void deleteNode(Node *root) {
     //TODO IMPLEMENT
+}
+
+bool isLeftChild(Node node) {
+    return node == node->parent->left;
+}
+
+bool isRightChild(Node node) {
+    return node == node->parent->right;
 }
 
 #endif //REDBLACKTREE_REDBLACKTREEFUNCTIONS_H
